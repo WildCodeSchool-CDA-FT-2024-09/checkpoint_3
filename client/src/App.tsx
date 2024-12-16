e4702e1f2757d0d8fe86a44399eb2b4abe293416
Fabien Amann <fabien@feub.net>
2024-12-16 10:17:18 +0100

    client: added basic header/navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,48 @@
 import { Outlet } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Nav from "./components/Nav";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+    h1: {
+      fontSize: "1.5rem",
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: "1.25rem",
+      fontWeight: 600,
+    },
+  },
+  palette: {
+    primary: {
+      main: "#00235B", // Blue
+      // light: will be calculated from palette.primary.main,
+      // dark: will be calculated from palette.primary.main,
+      contrastText: "#F5F5F5",
+    },
+    secondary: {
+      main: "#D2DEF1", // Light blue
+      contrastText: "#00235B",
+    },
+    success: {
+      main: "#6EBF8B", // Green
+      contrastText: "#FFFFFF",
+    },
+    error: {
+      main: "#E21818", // Red
+      contrastText: "#FFFFFF",
+    },
+  },
+});
 
 function App() {
   return (
     <>
-      <h1>NAV</h1>
-      <Outlet />
+      <ThemeProvider theme={theme}>
+        <Nav />
+        <Outlet />
+      </ThemeProvider>
     </>
   );
 }
---
+++ client/src/components/Nav.tsx
@@ -0,0 +1,18 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h1" component="div" sx={{ flexGrow: 1 }}>
+            Checkpoint: frontend - countries
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
