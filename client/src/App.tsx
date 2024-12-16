e483fe31d55a25bdacb85fdad1bd5379a42bf12e
JohaneDecamps <johane.decamps@gmail.com>
2024-12-16 11:27:39 +0100

    features-navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <main>
-      <Outlet />
-    </main>
+    <>
+      <NavBar />
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 }
 
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+import "./NavBar.css"
+
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <h1> Checkpoint 3 </h1>
+      <h3> Countries </h3>
+    </nav>
+  );
+}
