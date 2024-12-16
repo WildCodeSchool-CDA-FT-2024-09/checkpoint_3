5fd7e201ae0b431c61152e4be38482dfe0084707
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2024-12-16 10:41:12 +0100

    1/Config CSS + 2/Config vite react

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+import connection from "./services/connection.ts";
+import App from "./App.tsx";
+import "./index.css";
+import Country from "./pages/Country.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        path: "/country",
+        element: <Country />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ApolloProvider client={connection}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
+  </StrictMode>
+);
