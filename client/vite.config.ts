861034c649b16c7379134c5098febdf1d185748e
julien44830 <deniaudjulien0@gmail.com>
2024-12-16 09:37:29 +0100

    vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173, // inner port for Vite
+    watch: {
+      usePolling: true // Enable polling for file changes inside Docker
+    },
+    // hmr: {
+    //   host: 'localhost',
+    //   clientPort: 5173 // outer port for HMR
+    // }
+  },
+
   plugins: [react()],
 })
