19a80225d8b30381b505c09ce33f198f61832cac
scyanth <34488277+scyanth@users.noreply.github.com>
2024-12-16 12:14:36 +0100

    fetch back liste pays

--- client/src/components/CountriesList.tsx
+++ client/src/components/CountriesList.tsx
@@ -1,17 +1,27 @@
 import Grid from "@mui/material/Grid2";
 import Box from "@mui/material/Box";
 import CountryCard from "./CountryCard";
+import { useCountriesQuery } from "../generated/graphql-types";
+
+type Country = {
+  name: string;
+  emoji: string;
+};
 
 export default function CountriesList() {
+  const { loading, error, data } = useCountriesQuery();
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error !</h1>;
+
   return (
     <Box sx={{ flexGrow: 1, padding: 2 }}>
       <Grid container spacing={2}>
-        <Grid size={{ xs: 12, lg: 6 }}>
-          <CountryCard name="France" emoji="🇫🇷" />
-        </Grid>
-        <Grid size={{ xs: 12, lg: 6 }}>
-          <CountryCard name="China" emoji="🇨🇳" />
-        </Grid>
+        {data!.countries.map((country: Country) => (
+          <Grid size={{ xs: 12, lg: 6 }}>
+            <CountryCard name={country.name} emoji={country.emoji} />
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ApolloProvider } from "@apollo/client";
+import connexion from "./services/connexion";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ApolloProvider client={connexion}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>
+);
