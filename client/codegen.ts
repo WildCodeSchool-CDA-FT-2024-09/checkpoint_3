9951b2209ea9e56c76b514c6d30f6751e9b6db9a
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-12-16 10:03:10 +0100

    Step 3: graphQL - codegen env setup

---
+++ client/codegen.ts
@@ -0,0 +1,24 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: 'http://localhost:4000/graph',
+  documents: ['src/schema/*.ts'],
+  generates: {
+    './src/generated/graphql-types.ts': {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-react-apollo'
+      ],
+      config: {
+        withHooks: true,
+        scalars: {
+          DateTime: Date
+        }
+      }
+    }
+  },
+  overwrite: true
+};
+
+export default config;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import client from "./services/connexion.ts";
+import { ApolloProvider } from '@apollo/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
   </StrictMode>,
 )
