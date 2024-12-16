b8ef25b1ed735e65c6bb33a56fa5420908cfec21
Fabien Amann <fabien@feub.net>
2024-12-16 10:09:09 +0100

    client: pages structure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,44 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import connection from "./services/connection.ts";
 import App from "./App.tsx";
+import Home from "./pages/Home.tsx";
+import Country from "./pages/Country.tsx";
 import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Outlet />,
+        children: [
+          {
+            index: true,
+            element: <Home />,
+          },
+          {
+            path: "country/:id",
+            element: <Country />,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ApolloProvider client={connection}>
-      <App />
+      <RouterProvider router={router} />
     </ApolloProvider>
   </StrictMode>,
 );
