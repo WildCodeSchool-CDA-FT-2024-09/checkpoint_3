5ae5064827712d53b6540aa6b73c42ed4d457f68
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-12-16 11:23:31 +0100

    added query to country details page

--- client/src/pages/Country/Country.tsx
+++ client/src/pages/Country/Country.tsx
@@ -1,7 +1,19 @@
 import { useParams } from "react-router-dom";
+import { useCountryQuery } from "../../generated/graphql-types";
 
 export default function Country() {
   const { countryCode } = useParams();
-
-  return <p>country {countryId}</p>;
+  const { data } = useCountryQuery({ variables: { code: countryCode } });
+  if (data)
+    return (
+      <>
+        <p>{data.country.emoji}</p>
+        <p>
+          Name : {data.country.name} ({countryCode})
+        </p>
+        <p>
+          Continent : {data.country.continent?.name}
+        </p>
+      </>
+    );
 }
