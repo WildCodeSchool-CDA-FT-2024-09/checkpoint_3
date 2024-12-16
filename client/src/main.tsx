0c986579895bd99ef5739888f09a739326dc2048
Julien RICHARD <julien.y.richard@gmail.com>
2024-12-16 14:57:43 +0100

    Intégration de graphClient et apollo, test dans APP

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+import client from "./services/client.ts";
 import router from "./routes/router";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
   </StrictMode>
 );
