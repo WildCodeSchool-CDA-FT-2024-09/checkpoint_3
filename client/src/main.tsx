26dc37016e031fc6f3c792d3a663a81325eb59e8
Fabien Amann <fabien@feub.net>
2024-12-16 09:58:48 +0100

    client: installed MUI + project basic cleanup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ApolloProvider } from "@apollo/client";
 import connection from "./services/connection.ts";
-import "./index.css";
 import App from "./App.tsx";
+import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
